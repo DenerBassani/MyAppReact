fb43ed7a7f9c36d717e5526e14ba9533773f3b58
DenerBassani <dede.bassanidesouza@gmail.com>
2025-04-03 20:52:43 -0300

    Aula 2 - Init Project
    Init Project + Components + Pages + Styles

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,10 +1,28 @@
 import logo from './logo.svg';
 import './App.css';
+import { Plus } from 'react-feather';
+import styles from './styles/pages/home.module.css';
+import { Task } from './components/Task';
 
 function App() {
   return (
     <div className="App">
-      Hello World
+      <section className={styles.container}>
+        <header className={styles.header}>
+         
+        <h1>Minhas Tarefas</h1>
+        
+        <button type="button" className={styles.addTaskButton}>
+          Adicionar uma nova tarefa <Plus/>
+        </button>
+
+        </header>
+        <div>
+          <Task/>
+        </div>    
+      </section>
+
+
     </div>
   );
 }
